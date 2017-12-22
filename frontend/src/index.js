90a60e13168dc019df3eeee2053c1945cb875410
Divyank Vijayvergiya <divyankvijay.96@gmail.com>
2017-12-23 00:44:31 +0530

    Import all the necessary files initially

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
 import { Provider } from 'react-redux'
+import { createStore, applyMiddleware} from 'redux';
 import './index.css';
-import App from './App';
+import App from './containers/App'
+import  thunk from redux-thunk;
+import reducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
+const store = createStore(
+  reducers,
+  {},
+  applyMiddleware(thunk)
+);
 ReactDOM.render(
   <Provider store={store}>
+  <BrowserRouter>
     <App/>
+  </BrowserRouter>
   </Provider>, document.getElementById('root'));
 registerServiceWorker();
